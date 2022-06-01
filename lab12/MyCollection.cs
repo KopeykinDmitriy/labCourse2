7a6feea9455c758aaf6707f6bc35010586fb9d45
KopeykinDmitriy <78786068+KopeykinDmitriy@users.noreply.github.com>
2022-06-01 17:50:29 +0500

    Create MyCollection.cs

---
+++ lab12/MyCollection.cs
@@ -0,0 +1,193 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using lab_10;
+
+namespace lab_12
+{
+    public class MyCollection<T> : IEnumerable<T>, ICollection<T>, ICloneable where T : ICloneable, IComparable
+    {
+        public int Count { get; private set; } = 0;
+
+        public virtual T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count) throw new IndexOutOfRangeException();
+
+                CollectionPoint<T> cursor = First;
+
+                while (index-- != -1) cursor = cursor.Next;
+
+                return cursor.Value;
+            }
+            set
+            {
+                if (index < 0 || index >= Count) throw new IndexOutOfRangeException();
+
+                CollectionPoint<T> cursor = First;
+
+                while (index-- != 0) cursor = cursor.Next;
+
+                cursor.Value = value;
+            }
+        }
+
+        public bool IsReadOnly { get; private set; } = false;
+
+        public CollectionPoint<T> First { get; private set; } = default;
+
+        public CollectionPoint<T> Last { get; private set; } = default;
+
+        public MyCollection() { }
+
+        public MyCollection(MyCollection<T> collection)
+        {
+            foreach (var el in collection) Add((T)el.Clone());
+        }
+
+        
+        public virtual void Add(T value)
+        {
+            if (First is null) First = Last = new CollectionPoint<T>(value);
+            else
+            {
+                CollectionPoint<T> current = First;
+                while (current.Next != null) current = current.Next;
+
+                current.Next = new CollectionPoint<T>(value);
+                current.Next.Prev = current;
+                Last = current.Next;
+            }
+            Count++;
+        }
+
+        public void Add(T[] values)
+        {
+            if (!IsReadOnly) for (int i = 0; i < values.Length; i++) Add(values[i]);
+        }
+
+        public T Remove()
+        {
+            if (First == null || IsReadOnly) return default;
+            else
+            {
+                CollectionPoint<T> current = Last;
+
+                if (Count == 1) Last = First = null;
+                else
+                {
+                    Last = current.Prev;
+                    current.Prev.Next = null;
+                    current.Prev = null;
+                }
+
+                Count--;
+                return current.Value;
+            }
+        }
+
+       
+
+        public void Clear()
+        {
+            if (!IsReadOnly)
+            {
+                First = Last = null;
+                Count = 0;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator() 
+        {
+            CollectionPoint<T> current = Last;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Prev;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+       
+        public object Clone()
+        {
+            MyCollection<T> tmp = new MyCollection<T>();
+
+            foreach (var el in this) tmp.Add((T)el.Clone());
+
+            return tmp;
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (T element in this)
+                if (item.Equals(element)) return true;
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            int i = arrayIndex;
+
+            foreach (T element in this)
+            {
+                if (i == array.Length) break;
+                array[i++] = (T)element.Clone();
+            }
+        }
+
+        public virtual bool Remove(T item)
+        {
+            if (First == null || IsReadOnly) return false;
+            else
+            {
+                if (Count == 1 && First.Value.Equals(item)) Remove();
+                else
+                {
+                    CollectionPoint<T> cursor = First;
+                    while (cursor != null && !cursor.Value.Equals(item)) cursor = cursor.Next;
+                    if (cursor == null) return false;
+                    if (cursor.Next != null) cursor.Next.Prev = cursor.Prev;
+                    if (cursor.Prev != null) cursor.Prev.Next = cursor.Next;
+                    Count--;
+                }
+                return true;
+            }
+        }
+
+        public virtual void Show()
+        {
+            if (First != null)
+            {
+                CollectionPoint<T> cursor = First;
+                while (cursor != null)
+                {
+                    Console.WriteLine(cursor.Value);
+                    cursor = cursor.Next;
+                }
+            }
+        }
+    }
+
+    public class CollectionPoint<T>
+    {
+        public T Value { get; set; } = default;
+
+        public CollectionPoint<T> Next { get; set; } = default;
+
+        public CollectionPoint<T> Prev { get; set; } = default;
+
+        public CollectionPoint(T value) => Value = value;
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
